3752294485dd7fe4f5bbb2a9955fd29c1a9fc13f
Dat Bac Do <dat.b.do@gmail.com>
2016-10-29 11:20:09 -0400

    test

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function parseRedirectQuery(query, replace) {
 const gitHubRepoName = 'CSSattendance';
 // The domain for your site
 // SET THIS: e.g. http://subdomain.example.tld, or http://www.example.tld
-const domain = 'http://csswit.github.io';
+const domain = 'http://csswit.github.io/CSSattendance';
 function redirectToDomain() {
   window.location.replace(domain);
 }
